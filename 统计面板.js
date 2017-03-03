6744b27308856bfa3c52fc8208b1fcbaa8b35100
李正强 <lizhengqiang@mouge.wang>
2017-03-03 21:39:10 +0800

    Create 统计面板.js

---
+++ 统计面板.js
@@ -0,0 +1,63 @@
+{
+  label: "统计面板",
+  funcs: {
+      datacube: function (ctx, api, mapper) {
+          var self = ctx("this")
+          ctx("http")("get", "/api/mp/dynamic/cgi", {
+              params: {
+                  mpId: ctx("mp").id,
+                  api: api,
+                  data: JSON.stringify({
+                      "begin_date": self.config.children.beginDate.value,
+                      "end_date": self.config.children.endDate.value
+                  })
+              }
+          }).then(function (resp) {
+              var list = (JSON.parse(resp.json().data)).list
+              ctx("this").config.children.resultList.items = list.map(mapper)
+
+          })
+      }
+  },
+  children: {
+      beginDate: {
+          type: "InputItem",
+          label: "开始时间",
+          value: "2017-03-01",
+      },
+      endDate: {
+          type: "InputItem",
+          label: "结束时间",
+          value: "2017-03-02",
+      },
+      resultList: {
+          type: "ListItem",
+          label: "列表",
+          items: [],
+      },
+
+      userSummaryButton: {
+          name: "test",
+          type: "ButtonItem",
+          label: "用户增长",
+          func: function (ctx) {
+              var self = ctx("this")
+              self.config.funcs.datacube(ctx, "datacube/getusersummary", function (item) {
+                  return JSON.stringify(item)
+              })
+          }
+      },
+
+      userCumulateButton: {
+          name: "test",
+          type: "ButtonItem",
+          label: "用户总数",
+          func: function (ctx) {
+              var self = ctx("this")
+              self.config.funcs.datacube(ctx, "datacube/getusercumulate", function (item) {
+                  return JSON.stringify(item)
+              })
+          }
+      }
+  }
+}
