05660eb557ec0e616a4b5b644b3ee37ba35902be
lizhengqiang <lizhengqiang@mouge.wang>
2017-03-05 14:29:52 +0800

    tag

--- 标签管理.js
+++ 标签管理.js
@@ -1,63 +1,37 @@
 eval({
-  label: "标签管理",
-  funcs: {
-      datacube: function (ctx, api, mapper) {
-          var self = ctx("this")
-          ctx("http")("get", "/api/mp/dynamic/cgi", {
-              params: {
-                  mpId: ctx("mp").id,
-                  api: api,
-                  data: JSON.stringify({
-                      "begin_date": self.config.children.beginDate.value,
-                      "end_date": self.config.children.endDate.value
-                  })
-              }
-          }).then(function (resp) {
-              var list = (JSON.parse(resp.json().data)).list
-              ctx("this").config.children.resultList.items = list.map(mapper)
+    label: "标签管理",
+    funcs: {
+        get: function (ctx, mapper) {
+            var self = ctx("this")
+            ctx("http")("get", "/api/mp/dynamic/cgi", {
+                params: {
+                    mpId: ctx("mp").id,
+                    api: "cgi-bin/groups/get",
+                    data: JSON.stringify({})
+                }
+            }).then(function (resp) {
+                var list = (JSON.parse(resp.json().data)).groups
+                ctx("this").config.children.resultList.items = list.map(mapper)
 
-          })
-      }
-  },
-  children: {
-      beginDate: {
-          type: "InputItem",
-          label: "开始时间",
-          value: "2017-03-01",
-      },
-      endDate: {
-          type: "InputItem",
-          label: "结束时间",
-          value: "2017-03-02",
-      },
-      resultList: {
-          type: "ListItem",
-          label: "列表",
-          items: [],
-      },
-
-      userSummaryButton: {
-          name: "test",
-          type: "ButtonItem",
-          label: "用户增长",
-          func: function (ctx) {
-              var self = ctx("this")
-              self.config.funcs.datacube(ctx, "datacube/getusersummary", function (item) {
-                  return JSON.stringify(item)
-              })
-          }
-      },
-
-      userCumulateButton: {
-          name: "test",
-          type: "ButtonItem",
-          label: "用户总数",
-          func: function (ctx) {
-              var self = ctx("this")
-              self.config.funcs.datacube(ctx, "datacube/getusercumulate", function (item) {
-                  return JSON.stringify(item)
-              })
-          }
-      }
-  }
+            })
+        }
+    },
+    children: {
+        resultList: {
+            type: "ListItem",
+            label: "列表",
+            items: [],
+        },
+        getButton: {
+            name: "test",
+            type: "ButtonItem",
+            label: "全部标签",
+            func: function (ctx) {
+                var self = ctx("this")
+                self.config.funcs.get(ctx, function (item) {
+                    return JSON.stringify(item)
+                })
+            }
+        },
+    }
 })
