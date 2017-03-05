eab15bde9bb7023d35a4edf768656b548200abec
lizhengqiang <lizhengqiang@mouge.wang>
2017-03-05 14:51:40 +0800

    src

--- 标签管理.js
+++ 标签管理.js
@@ -1,37 +1,58 @@
 eval({
     label: "标签管理",
     funcs: {
-        get: function (ctx, mapper) {
+        get: function (ctx, url, data, handler) {
             var self = ctx("this")
             ctx("http")("get", "/api/mp/dynamic/cgi", {
                 params: {
                     mpId: ctx("mp").id,
-                    api: "cgi-bin/groups/get",
-                    data: JSON.stringify({})
+                    api: url,
+                    data: JSON.stringify(data)
                 }
             }).then(function (resp) {
-                var list = (JSON.parse(resp.json().data)).groups
-                ctx("this").config.children.resultList.items = list.map(mapper)
+                var list = (JSON.parse(resp.json().data))
+                handler(list)
 
             })
         }
     },
     children: {
-        resultList: {
-            type: "ListItem",
-            label: "列表",
-            items: [],
-        },
         getButton: {
             name: "test",
             type: "ButtonItem",
             label: "全部标签",
             func: function (ctx) {
                 var self = ctx("this")
-                self.config.funcs.get(ctx, function (item) {
-                    return JSON.stringify(item)
+                self.config.funcs.get(ctx, "cgi-bin/groups/get", {}, function (data) {
+                    self.config.children.groups.options = data.groups.map(function (item) {
+                        return item;
+                    })
                 })
             }
         },
+        groups: {
+            type: "SelectItem",
+            label: "分组",
+            options: []
+        },
+        //getUserButton: {
+        //    name: "test",
+        //    type: "ButtonItem",
+        //    label: "用户列表",
+        //    func: function (ctx) {
+        //        var self = ctx("this")
+        //        self.config.funcs.get(ctx, "cgi-bin/user/get", {}, function (data) {
+        //            self.config.children.resultList.items = data.data.openid.map(function (item) {
+        //                return item;
+        //            })
+        //        })
+        //    }
+        //},
+        resultList: {
+            type: "ListItem",
+            label: "列表",
+            items: [],
+        }
+
     }
 })
